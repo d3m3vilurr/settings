47de8a6983878f7903839b71ed0b9dc3f532725b
d3m3vilurr <d3m3vilurr@gmail.com>
2011-06-14 14:52:48 +0900

    added git-flow-completion submodule

--- .profile.d/bash_completion.sh
+++ .profile.d/bash_completion.sh
@@ -7,3 +7,12 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
 
+if [ -d ~/.bash_completion.d ]; then
+  for i in ~/.bash_completion.d/*.bash; do
+    if [ -r $i ]; then
+      . $i
+    fi
+  done
+  unset i
+fi
+
