5f2dafe575023b497b69a5bcfdd6260584b849d1
d3m3vilurr <d3m3vilurr@gmail.com>
2011-10-16 23:25:59 +0900

    multiline prompt

--- .profile.d/repo_prompt.sh
+++ .profile.d/repo_prompt.sh
@@ -50,5 +50,5 @@ branch_color ()
    echo -ne $color
 }
 
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_branch)\$ '
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_branch)\n\$ '
 
