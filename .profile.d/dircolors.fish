2cb333631718b5a4dba09e8153f13c6a08096710
Sunguk Lee <d3m3vilurr@gmail.com>
2014-02-24 08:32:35 +0900

    Now, solarized support tty console

--- .profile.d/dircolors.fish
+++ .profile.d/dircolors.fish
@@ -1,5 +1,14 @@
-if not set -q LS_COLORS
-        and type -f dircolors > /dev/null
-        and type -f ~/.dir_colors > /dev/null
-    eval (dircolors -c ~/.dir_colors | sed 's/>&\/dev\/null$//')
+switch (echo $TERM)
+    case linux
+        if not set -q LS_COLORS
+                and type -f dircolors > /dev/null
+                and type -f ~/.dircolors.ansi > /dev/null
+            eval (dircolors -c ~/.dircolors.ansi | sed 's/>&\/dev\/null$//')
+        end
+    case '*'
+        if not set -q LS_COLORS
+                and type -f dircolors > /dev/null
+                and type -f ~/.dircolors.256 > /dev/null
+            eval (dircolors -c ~/.dircolors.256 | sed 's/>&\/dev\/null$//')
+        end
 end
---
+++ .profile.d/term.fish
@@ -0,0 +1,25 @@
+switch (echo $TERM)
+    case linux
+        echo -en "\e]P0002b36" # console background (dark blue)
+        echo -en "\e]P7839496" # console foreground text (light grey)
+        echo -en "\e]PC2aa198" # ls directory color, vim message color (cyan)
+
+        echo -en "\e]P593a1a1" # xresources keywords
+        echo -en "\e]P3268bd2" # xresources key names
+
+        echo -en "\e]P9cb4b16" # vim variables
+        echo -en "\e]P62aa198" # xresources values, vim strings, search bg color
+        echo -en "\e]PA586e75" # vim comments
+        echo -en "\e]P2859900" # vim keywords, selection bg, bracket matching bg
+        echo -en "\e]P1dc322f" # vim escape characters, whitespace in git show, untracked files in git status
+        echo -en "\e]PB657b83" # vim empty line markers ~
+        echo -en "\e]P4cb4b16" # vim constants, cursor, mode indicator, sqbracket match fg
+        echo -en "\e]P8073642" # vim selection fg
+
+        echo -en "\e]PF859900" # command executed in git show
+
+        echo -en "\e]PDd33682" # magenta
+        echo -en "\e]PE0000ff" # debug blue
+    case xterm
+        set -x TERM 'xterm-256color'
+end
