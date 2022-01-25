799e6e3a8cf963b3cb61935a7001645b2e19862e
Sunguk Lee <d3m3vilurr@gmail.com>
2022-01-25 16:33:52 +0900

    profile.d: Fix wrong and conditions

--- .profile.d/dircolors.fish
+++ .profile.d/dircolors.fish
@@ -1,11 +1,11 @@
 function _dircolors_set_256_color
-    if type -fq dircolors and; test -e ~/.dircolors.256
+    if type -fq dircolors; and test -e ~/.dircolors.256
         eval (dircolors -c ~/.dircolors.256 | sed 's/>&\/dev\/null$//')
     end
 end
 
 function _dircolors_set_ansi_color
-    if type -fq dircolors and; test -e ~/.dircolors.ansi
+    if type -fq dircolors; and test -e ~/.dircolors.ansi
         eval (dircolors -c ~/.dircolors.ansi | sed 's/>&\/dev\/null$//')
     end
 end
